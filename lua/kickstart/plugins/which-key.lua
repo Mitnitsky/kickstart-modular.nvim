614524a6d020722ac8c8f23e645d68989a5682e8
Vladimir Mitnitsky <vmitnitsky@microsoft.com>
2025-03-18 12:17:35 +0200

    init of my config

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -59,6 +59,9 @@ return {
 
       -- Document existing key chains
       spec = {
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>D', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
